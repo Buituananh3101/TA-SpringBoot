66eb9bc36bc871d6fd496399a7c7afffd5ad135c
Buituananh3101 <31012006github@gmail.com>
2026-02-13 06:06:02 +0700

    @PathVariables

---
+++ s0707-ConvertPojoToJson/src/main/java/com/example/s0705/S0705Application.java
@@ -0,0 +1,13 @@
+package com.example.s0705;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S0705Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S0705Application.class, args);
+	}
+
+}
