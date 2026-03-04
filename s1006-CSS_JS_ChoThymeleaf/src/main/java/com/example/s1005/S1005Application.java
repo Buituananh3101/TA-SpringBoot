139adb5f969405ca4ad2239f6f4416a9998e1e6f
Buituananh3101 <31012006github@gmail.com>
2026-03-04 10:42:15 +0700

    cre

---
+++ s1006-CSS_JS_ChoThymeleaf/src/main/java/com/example/s1005/S1005Application.java
@@ -0,0 +1,13 @@
+package com.example.s1005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1005Application.class, args);
+	}
+
+}
