e3c302a37dac551f3ec387deb1866c69e1d6aa4c
Buituananh3101 <31012006github@gmail.com>
2026-03-04 10:09:40 +0700

    Tạo

---
+++ s1005-Thymeleaf/src/test/java/com/example/s1005/S1005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s1005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
