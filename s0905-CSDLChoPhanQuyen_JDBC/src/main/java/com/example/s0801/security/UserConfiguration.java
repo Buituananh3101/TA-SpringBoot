71295945c74f3225097fdddb3fd08609f9fdbe71
Buituananh3101 <31012006github@gmail.com>
2026-03-04 09:13:24 +0700

    Hiểu

--- s0905-CSDLChoPhanQuyen_JDBC/src/main/java/com/example/s0801/security/UserConfiguration.java
+++ s0905-CSDLChoPhanQuyen_JDBC/src/main/java/com/example/s0801/security/UserConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.s0801.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,42 +10,51 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class UserConfiguration {
 
-    // Tạo các user
+    // Có tk này thì ko cần InMemoryUserDetailsManager nữa
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        UserDetails teach = User
-                .withUsername("teacher")
-                .password("{noop}123123") // Nhớ có cái {noop}
-                .roles("teacher")
-                .build();
-        UserDetails learn = User
-                .withUsername("student")
-                .password("{noop}123123")
-                .roles("student")
-                .build();
-        UserDetails admin = User
-                .withUsername("admin")
-                .password("{noop}admin123")
-                .roles("admin")
-                .build();
-        return new InMemoryUserDetailsManager(teach, learn, admin);
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+//    // Tạo các user
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+//        UserDetails teach = User
+//                .withUsername("teacher")
+//                .password("{noop}123123") // Nhớ có cái {noop}
+//                .roles("teacher")
+//                .build();
+//        UserDetails learn = User
+//                .withUsername("student")
+//                .password("{noop}123123")
+//                .roles("student")
+//                .build();
+//        UserDetails admin = User
+//                .withUsername("admin")
+//                .password("{noop}admin123")
+//                .roles("admin")
+//                .build();
+//        return new InMemoryUserDetailsManager(teach, learn, admin);
+//    }
 
     // Phân quyền user xem ai đc vào api nào
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 configurer->configurer
-                        .requestMatchers(HttpMethod.GET,"/api/students").hasAnyRole("student", "teacher", "admin")
-                        .requestMatchers(HttpMethod.GET,"/api/students/**").hasAnyRole("student", "teacher", "admin")
-                        .requestMatchers(HttpMethod.POST,"/api/students").hasAnyRole("teacher","admin")
-                        .requestMatchers(HttpMethod.PUT,"/api/students").hasAnyRole( "teacher","admin")
-                        .requestMatchers(HttpMethod.DELETE,"/api/students/**").hasRole( "admin")
+                        .requestMatchers(HttpMethod.GET,"/api/students").hasAnyRole("TEACHER", "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/api/students/**").hasAnyRole("TEACHER", "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/api/students").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/api/students").hasAnyRole( "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/api/students/**").hasRole( "ADMIN")
         );
         http.httpBasic(Customizer.withDefaults());
 //        http.csrf(csrf->csrf.disable()); // chống giả mạo // hình như dòng bên dưới là bản cập nhật
