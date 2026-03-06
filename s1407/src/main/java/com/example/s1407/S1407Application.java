a369785b207b3bebb540570cf2413f1032a8f2b0
Buituananh3101 <31012006github@gmail.com>
2026-03-06 13:16:33 +0700

    Tạo

---
+++ s1407/src/main/java/com/example/s1407/S1407Application.java
@@ -0,0 +1,13 @@
+package com.example.s1407;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1407Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1407Application.class, args);
+	}
+
+}
